19c2e629b40d381c06942689774de14ed52173f2
ljm <ljm@addcn.com>
2020-06-15 10:42:33 +0800

    添加例子

---
+++ example/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+export default {
+  middleware: [
+    (routing, next) => {
+      const bool=true;
+      if (bool) {
+        console.log("没有权限无法进入", routing, next);
+        routing.router.replace("/")
+      } else {
+        next();
+      }
+    },
+    (routing, next) => {
+      console.log("进入第二个中间件", routing, next);
+      next();
+    }
+  ]
+};
+</script>
---
+++ example/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  middleware:[(routing,next)=>{
+    console.log("中间件一");
+    next()
+  },(routing,next)=>{
+    console.log("中间件二",routing,next);
+    next()
+  }]
+}
+</script>
